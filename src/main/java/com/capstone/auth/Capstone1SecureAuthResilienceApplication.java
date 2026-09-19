e575697143476a9958a11a349080fcf3e4324867
Soham07-Coder <sohamgore2005@gmail.com>
2026-09-20 01:03:07 +0530

    Added Circuit Breaker On DB Connections and handled Some Exceptions for MethodArgument And Also Made Chnages in AuthIml to Integarte Circuit Breaker

---
+++ src/main/java/com/capstone/auth/Capstone1SecureAuthResilienceApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Capstone1SecureAuthResilienceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Capstone1SecureAuthResilienceApplication.class, args);
+    }
+
+}
